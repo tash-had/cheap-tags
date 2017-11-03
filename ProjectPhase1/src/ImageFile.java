b0219d7c247672e5b95354947c3f46d72b5b4525
Renjie Li <renjie6li@gmail.com>
2017-11-03 17:31:57 -0400

    add ImageFile

---
+++ ProjectPhase1/src/ImageFile.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class ImageFile{
+    private String currentName;
+    private String oldName;
+    private ArrayList<String> timestamp;
+    public ImageFile(String actual_file_name){
+        currentName = actual_file_name;
+        oldName = "";
+    }
+    public void change_image_name(String newName){
+        oldName =  currentName;
+        currentName = newName;
+
+    }
+}
