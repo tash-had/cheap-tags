b16e78e9a193a91e68a7a5263238976b80b5db63
Renjie Li <renjie6li@gmail.com>
2017-11-03 17:21:55 -0400

    rename the Imagefile to ImageFile

--- ProjectPhase1/src/Imagefile.java
+++ ProjectPhase1/src/Imagefile.java
@@ -1,10 +1,15 @@
-public class  Imagefile{
-    private String currentName = "";
-    private String oldName = "";
-    public Imagefile(String actual_file_name){
+import java.util.ArrayList;
+public class ImageFile{
+    private String currentName;
+    private String oldName;
+    private ArrayList<String> timestamp;
+    public ImageFile(String actual_file_name){
         currentName = actual_file_name;
+        oldName = "";
     }
     public void change_image_name(String newName){
-
+        oldName =  currentName;
+        currentName = newName;
     }
+    public
 }
