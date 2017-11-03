2fa00dfc51189ed05969eb3a9baa9af623ecb59a
Tash-had Saqif <me@tash-had.com>
2017-11-03 17:37:34 -0400

    Added getNewId function to FileManager

--- ProjectPhase1/src/FileManager.java
+++ ProjectPhase1/src/FileManager.java
@@ -1,8 +1,18 @@
 import java.util.HashMap;
+import java.util.UUID;
 
 /**
  * Created by tash-had on 2017-11-03.
  */
 public class FileManager {
-    public static HashMap<String, ImageFile>
+    public static HashMap<String, ImageFile> idToImage;
+    public static HashMap<String, String> imageNameToId;
+
+    static String getNewId(){
+        return UUID.randomUUID().toString();
+    }
+
+
 }
+
+
--- ProjectPhase1/src/ImageFile.java
+++ ProjectPhase1/src/ImageFile.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+
 public class ImageFile{
     private String currentName;
     private String oldName;
