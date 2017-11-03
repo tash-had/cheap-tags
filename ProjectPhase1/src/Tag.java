ddbf810412053d21a02c0b1d3bef4b450bd80387
carolineming1101@gmail.com <carolineming1101@gmail.com>
2017-11-03 17:27:58 -0400

    Modified class name

---
+++ ProjectPhase1/src/Tag.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class Tag {
+    private String _name;
+    public static ArrayList<Imagefile> _images = new ArrayList<Imagefile>();
+
+    public Tag(String name) {
+        _name = name;
+
+    }
+    
+}
+
