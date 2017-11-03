3a66d64a8ad687b8d48059d075a25270e34aaeb1
carolineming1101@gmail.com <carolineming1101@gmail.com>
2017-11-03 17:34:21 -0400

    Tag -Caroline

---
+++ ProjectPhase1/src/Tag.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class Tag {
+    private String _name;
+    public static ArrayList<ImageFile> _images = new ArrayList<ImageFile>();
+
+    public Tag(String name) {
+        _name = name;
+
+    }
+
+}
