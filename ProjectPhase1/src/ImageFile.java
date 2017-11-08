4b490b4db1f4a87822ecfeea8f308544c6efb59c
Renjie Li <renjie6li@gmail.com>
2017-11-08 16:41:03 -0500

    fix the imagefile.java

--- ProjectPhase1/src/ImageFile.java
+++ ProjectPhase1/src/ImageFile.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class ImageFile{
-<<<<<<< HEAD
     private String currentName;
     //oldName keeps track of all of the revision histories in the format of array [newname,previous name,timestamp]
     private ArrayList<String[]> oldName;
@@ -13,18 +12,7 @@ public class ImageFile{
         currentName = actual_file_name;
         oldName = new ArrayList<String[]>();
         UUID = FileManager.getNewId(); //what parameter does FileManager.getNewId take???
-=======
-    public String currentName;
-    private String oldName;
 
-    private ArrayList<String> timestamp;
-    public String imageUUID;
-    public ImageFile(String actual_file_name){
-        currentName = actual_file_name;
-        oldName = "";
-        imageUUID = UUID.randomUUID().toString();
->>>>>>> ca6c5bb94f94d95adbdcb62cb54253b9dcd06201
-    }
     public void change_image_name(String newName){
         Long timestamp = System.currentTimeMillis();
         String[] templog = {newName,currentName,timestamp.toString()};
