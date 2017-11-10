0fe993544b44014118644e01053493e5e064ece8
Tash-had Saqif <me@tash-had.com>
2017-11-10 17:10:13 -0500

    commented out unfinished function

--- ProjectPhase1/src/FileOperationsManager.java
+++ ProjectPhase1/src/FileOperationsManager.java
@@ -13,29 +13,29 @@ public class FileOperationsManager {
     public void renameFile(Path path, String newName) throws IOException {
         Files.move(path, path.resolveSibling(newName));
     }
-
-    /**
-     * Change the directory to image
-     * @param newDirectory is the new directory where we will move file to)
-     */
-    public void changeImageDirectory(File fileToMove, Path newDirectory){
-        StringBuilder targetDirectory = new StringBuilder();
-        char tempchar = '\\';
-        String tempDirectory = newDirectory.toString();
-        for(int i=0;i<tempDirectory.length();i++){
-            if(tempDirectory.charAt(i)==tempchar){
-                targetDirectory.append("/");
-            }
-            else {
-                targetDirectory.append(tempDirectory.charAt(i));
-            }
-        }
-        tempDirectory = targetDirectory.toString() +"/" + this.previousName;
-        File tempfile = new File (tempDirectory);
-        this.movingSuccess=this.getthisFile().renameTo(tempfile);
-        this.thisFile = tempfile;
-        this.underWhichDirectory = tempfile.getParent();
-    }
+//
+//    /**
+//     * Change the directory to image
+//     * @param newDirectory is the new directory where we will move file to)
+//     */
+//    public void changeImageDirectory(File fileToMove, Path newDirectory){
+//        StringBuilder targetDirectory = new StringBuilder();
+//        char tempchar = '\\';
+//        String tempDirectory = newDirectory.toString();
+//        for(int i=0;i<tempDirectory.length();i++){
+//            if(tempDirectory.charAt(i)==tempchar){
+//                targetDirectory.append("/");
+//            }
+//            else {
+//                targetDirectory.append(tempDirectory.charAt(i));
+//            }
+//        }
+//        tempDirectory = targetDirectory.toString() +"/" + this.previousName;
+//        File tempfile = new File (tempDirectory);
+//        this.movingSuccess=this.getthisFile().renameTo(tempfile);
+//        this.thisFile = tempfile;
+//        this.underWhichDirectory = tempfile.getParent();
+//    }
 
     public void fetchFromDirectory(){
 
