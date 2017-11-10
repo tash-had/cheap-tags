0dcb9ad97a7282573426b6b632854976fcbd2939
Tash-had Saqif <me@tash-had.com>
2017-11-10 13:44:56 -0500

    Edit Main.java to work exclusively with the PrimaryStageManager

--- ProjectPhase1/src/Main.java
+++ ProjectPhase1/src/Main.java
@@ -1,67 +1,77 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.stage.DirectoryChooser;
 
-import java.io.File;
+import java.io.IOException;
 
 public class Main extends Application{
 
-    public Scene home;
-    public Stage window;
-
     @Override
-    public void start(Stage primaryStage) {
-        window = primaryStage;
+    public void start(Stage primaryStage) throws IOException {
+        PrimaryStageManager.setPrimaryStage(primaryStage);
+        PrimaryStageManager.setDefaultStageWidth(1080);
+        PrimaryStageManager.setDefaultStageHeight(720);
+        PrimaryStageManager.setScreen("Cheap Tags", "home_screen_view.fxml");
+        PrimaryStageManager.showPrimaryStage();
 
-        // Back button
-        Button back = new Button();
-        back.setText("Back");
-        back.setOnAction(e -> window.setScene(home));
+//        Parent root = FXMLLoader.load(getClass().getResource("home_screen_view.fxml"));
+//
+//        // Back button
+//        Button back = new Button();
+//        back.setText("Back");
+//        back.setOnAction(e -> mainStage.setScreen(home));
+//
+//        // Button to choose directory
+//        Button chooseDirectoryButton = new Button();
+//        chooseDirectoryButton.setText("Choose a directory");
+//        chooseDirectoryButton.setOnAction(e -> {
+//            DirectoryChooser directoryChooser=  new DirectoryChooser();
+//            directoryChooser.setTitle("Select a directory");
+//            File selectedFile = directoryChooser.showDialog(primaryStage);
+//            if (selectedFile != null) {
+//                mainStage.setScreen(ImageScene.constructImageScene(back));
+//            }
+//            else {
+//                mainStage.setScreen(home);
+//            }
+//        });
+//
+//        // Button to view my tags
+//        Button tagButton = new Button();
+//        tagButton.setText("My Tags");
+////        tagButton.setOnAction(e -> window.setScreen(DisplayTagsView.getScene(back));
+//
+//        Button back2 = new Button("go back!");
+//        tagButton.setOnAction(e -> mainStage.setScreen(DisplayTagsView.getScene(back2)));
+//        back2.setOnAction(e -> mainStage.setScreen(home));
+//
+//
+//        // constructs home layout
+//        StackPane homeLayout = new StackPane();
+//        homeLayout.getChildren().add(chooseDirectoryButton);
+//        homeLayout.getChildren().add(tagButton);
+//        chooseDirectoryButton.setTranslateY(0);
+//        tagButton.setTranslateY(50);
+//
+//        // constructs home scene
+//        home = new Scene(homeLayout, 300, 250);
+//        mainStage.setTitle("Main");
+////        mainStage.setScreen(home);
+//
+////        mainStage.setResizable(false);
+//        mainStage.setScreen(new Scene(root, 1080,720, Color.WHITE));
+//        mainStage.show();
 
-        // Button to choose directory
-        Button chooseDirectoryButton = new Button();
-        chooseDirectoryButton.setText("Choose a directory");
-        chooseDirectoryButton.setOnAction(e -> {
-            DirectoryChooser directoryChooser=  new DirectoryChooser();
-            directoryChooser.setTitle("Select a directory");
-            File selectedFile = directoryChooser.showDialog(primaryStage);
-            // if a file is selected, go to single image scene
-            if (selectedFile != null) {
-                //ImageFileManager.fetchFiles(selectedFile);
-                window.setScene(ImageScene.getImageScene(back));
-            }
-            // else, if nothing is selected, go back to home scene
-            else {
-                window.setScene(home);
-            }
-        });
-
-        // Button to view my tags
-        Button tagButton = new Button();
-        tagButton.setText("My Tags");
-
-        tagButton.setOnAction(e -> window.setScene(DisplayTagsView.getScene(back)));
-
-        // constructs home layout
-        StackPane homeLayout = new StackPane();
-        // adding directory and tag buttons
-        homeLayout.getChildren().add(chooseDirectoryButton);
-        homeLayout.getChildren().add(tagButton);
-        chooseDirectoryButton.setTranslateY(0);
-        tagButton.setTranslateY(50);
-
-        // constructs home scene
-        home = new Scene(homeLayout, 300, 250);
-        window.setTitle("Home");
-        window.setScene(home);
-        window.show();
+//        // constructs images layout
+//        StackPane imagesLayout = new StackPane();
+//        imagesLayout.getChildren().add(back);
+//
+//        // constructs images scene;
+//        images = new Scene(imagesLayout, 300, 250);
     }
     public static void main(String[] args){
         launch(args);
     }
+
+
 }
