78e728f10623516b4d9db33d1b6b756724a22f63
Tash-had Saqif <me@tash-had.com>
2017-11-14 00:55:51 -0500

    renamed FileOperationsManager to ImageFileOperationsManager and moved functions that don't deal specifically with ImageFiles to another class

---
+++ ProjectPhase1/src/managers/ImageFileOperationsManager.java
@@ -0,0 +1,109 @@
+package managers;
+
+import com.sun.istack.internal.Nullable;
+import javafx.scene.control.ButtonType;
+import model.ImageFile;
+import utils.Alerts;
+import utils.FileOperations;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
+/**
+ * This class manages how file operations for ImageFiles are handled
+ */
+public class ImageFileOperationsManager {
+
+    /**
+     * Rename a given ImageFile
+     *
+     * @param imageFile the image to rename
+     * @param newName the new name
+     * @return a File object with the new name if successfully renamed, the old File object otherwise
+     */
+    public static File renameImageFile(ImageFile imageFile, String newName){
+        File currentImageFile = imageFile.getThisFile();
+        String imageFilePath = currentImageFile.getParentFile().getAbsolutePath() +"/";
+        int renameStatus = FileOperations.renameFile(currentImageFile, newName);
+        if (renameStatus == 1){
+            UserDataManager.resetImageFileKey(currentImageFile.getName(), newName);
+            imageFilePath += newName;
+        }else if (renameStatus == -1){
+            String suffixedFileName = Alerts.showFileExistsAlert(currentImageFile.getParentFile(),
+                    new File(imageFilePath+newName),
+                    UserDataManager.getImageFileNames());
+            if (suffixedFileName != null){
+                /*
+                  TODO:
+                  Do steps required for a rename in imageFile
+                 */
+                renameImageFile(imageFile, suffixedFileName);
+            }else {
+                /*
+                  TODO:
+                  dont rename.
+                 */
+                return currentImageFile;
+            }
+        }else {
+            // Show error alert dialog
+            return currentImageFile;
+        }
+        return new File(imageFilePath);
+    }
+
+    /**
+     * Move an image to another directory
+     *
+     * @param imageFile the imageFile representing the image to move
+     * @return the
+     */
+    public static File moveImageFile(ImageFile imageFile) {
+        File oldFile = imageFile.getThisFile();
+        File newDirectory = PrimaryStageManager.getDirectoryWithChooser();
+        // A file object of the imagefile in the new directory
+        File newFile = new File(newDirectory.getAbsolutePath()+"/"+oldFile.getName());
+        int moveStatus = FileOperations.moveFile(oldFile, newDirectory.toPath());
+
+        if (moveStatus == 1){
+            return newFile;
+        }else if(moveStatus == -1){
+            String suffixedFileName = Alerts.showFileExistsAlert(newDirectory, newFile, null);
+            if (suffixedFileName != null){
+                /*
+                TODO:
+                Take steps to rename this imageFile to suffixedFile.getName()
+                (ie. setUnderWhichDirectory, imageFile.setFile, ...
+                Take steps to move this iamgeFile to suffixedFile.getParent()
+                 */
+//                newFile = new File(newDirectory.getAbsolutePath()+"/"+suffixedFileName);
+                // Make sure imageFile is renamed beforehand and its file attribute is reset with the new name before
+                // this line is run!
+                FileOperations.moveFile(imageFile.getThisFile(), newDirectory.toPath());
+            }else{
+                // Dont move
+                newFile = null;
+            }
+        }else {
+            // error alert
+        }
+        return newFile;
+    }
+
+    public void fetchFromDirectory() {
+
+    }
+
+    // Store data
+
+}
+
+
