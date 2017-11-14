6a4a653318c992484d2b7a92ebd9a2eb5b751af8
Tash-had Saqif <me@tash-had.com>
2017-11-13 21:06:21 -0500

    documented Alerts.showYesNoAlert and created Alerts.showFileExistsAlert

--- ProjectPhase1/src/utils/Alerts.java
+++ ProjectPhase1/src/utils/Alerts.java
@@ -1,6 +1,7 @@
 package utils;
 
 import com.sun.corba.se.spi.monitoring.LongMonitoredAttributeBase;
+import com.sun.istack.internal.Nullable;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -14,9 +15,14 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import managers.FileOperationsManager;
 
-//citing this from JavaFx Tut5th.
+import java.io.File;
+import java.util.Collection;
+
+
 public class Alerts {
+    //citing this from JavaFx Tut5th.
     public static void displayWarning(String message){
         Stage window = new Stage();
 
@@ -41,11 +47,48 @@ public class Alerts {
         //close to further process other things.
     }
 
+    /**
+     * Show an alert dialog with yes/no options
+     *
+     * @param title the dialog window title
+     * @param header the dialog header text
+     * @param body the dialog body text
+     * @return the user response to the dialog
+     */
     public static ButtonType showYesNoAlert(String title, String header, String body){
         Alert nameExistsDialog = new Alert(Alert.AlertType.CONFIRMATION, body, ButtonType.NO, ButtonType.YES);
         nameExistsDialog.setTitle(title);
         nameExistsDialog.setHeaderText(header);
         nameExistsDialog.showAndWait();
         return nameExistsDialog.getResult();
+    }
+
+    /**
+     * Tell the that they are trying to move/rename a file to/in a directory where a file with that name exists.
+     *
+     * @param directory the directory to/in which they are trying to move/rename a file
+     * @param file the file they are trying to move/rename
+     * @param filter which names to avoid when selecting a suffixed name
+     * @return
+     */
+    private static String showFileExistsAlert(File directory, File file, @Nullable Collection filter){
+        // Get new file with a suffix
+        String directoryPath = directory.getAbsolutePath()+"/";
+
+        String suffixedFileName = FileOperationsManager.getSuffixedFileName(file.getName(), directory);
+
+        while (filter != null && filter.contains(suffixedFileName)){
+            suffixedFileName = FileOperationsManager.getSuffixedFileName(suffixedFileName, directory);
+        }
+        // Ask user if they would like a suffixed name on the image
+        ButtonType renameReqResponse = Alerts.showYesNoAlert("Could not rename file",
+                "You Stupid",
+                file.getName() +" already exists. Would you like to name it "+suffixedFileName+"?");
+        if (renameReqResponse == ButtonType.OK){
+            System.out.println("Implement code IN THE CALLING METHOD, make this method return the modified file " +
+                    "after ImageFile implements a renameFunction");
+            return suffixedFileName;
+        }
+        return null;
     }
 }
