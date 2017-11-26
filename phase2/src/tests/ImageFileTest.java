b9de0646626f720a04d69e0f6d110d503198adf1
Renjie Li <renjie6li@gmail.com>
2017-11-26 16:47:01 -0500

    Wrote a Junit test for imagefile

--- phase2/src/tests/ImageFileTest.java
+++ phase2/src/tests/ImageFileTest.java
@@ -1,34 +1,34 @@
-//package tests;
-//
-//
-//import model.ImageFile;
-//import model.Tag;
-//import org.junit.Test;
-//import static org.junit.Assert.*;
-//
-//import java.io.File;
-//import java.util.ArrayList;
-//
-//public class ImageFileTest {
-//    @Test
-//    public void testGeneralRename(){
-//        File testFile = new File("src/resources/images/logo_2.jpg");
-//        ImageFile testImage = new ImageFile(testFile);
-//        testImage.generalReName("@Tag1 logo_2");
-//        assertEquals("");
-//
-//    }
-//
-//    @Test
-//    public void testGeneralRenameWithTwoParameter(){
-//        Tag t1 = new Tag("TAG1");
-//        Tag t2 = new Tag("TAG2");
-//        ArrayList<Tag> tempList = new ArrayList<Tag>();
-//        tempList.add(t1);
-//        tempList.add(t2);
-//        File testFile = new File("src/resources/images/logo_2.jpg");
-//        ImageFile testImage = new ImageFile(testFile);
-//
-//    }
-//
-//}
+package tests;
+
+
+import model.ImageFile;
+import model.Tag;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class ImageFileTest {
+    @Test
+    public void testGeneralRename(){
+        File testFile = new File("src/resources/images/logo_2.jpg");
+        ImageFile testImage = new ImageFile(testFile);
+        testImage.generalReName("@Tag1 logo_2");
+        assertEquals("@Tag1 logo_2",testImage.getCurrentName());
+    }
+
+    @Test
+    public void testGeneralRenameWithTwoParameter(){
+        Tag t1 = new Tag("TAG1");
+        Tag t2 = new Tag("TAG2");
+        ArrayList<Tag> tempList = new ArrayList<Tag>();
+        tempList.add(t1);
+        tempList.add(t2);
+        File testFile = new File("src/resources/images/logo_2.jpg");
+        ImageFile testImage = new ImageFile(testFile);
+        testImage.generalReName("@TAG1 @TAG2 logo_2",tempList);
+        assertEquals(t1,testImage.getTagList().get(0));
+    }
+
+}
