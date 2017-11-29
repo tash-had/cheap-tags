8477350ab7d9f1fcfc7953b5abd629011f7d3596
Caroline Ming <carolineming1101@gmail.com>
2017-11-29 18:24:35 -0500

    Added docstring to masterlogViewController, created a new class to manage all search bars

--- phase2/src/activities/MasterLogViewController.java
+++ phase2/src/activities/MasterLogViewController.java
@@ -18,18 +18,34 @@ import java.util.ResourceBundle;
 
 public class MasterLogViewController implements Initializable {
 
+    /**
+     * Shows a table for all revision history
+     */
     @FXML
     TableView<Log> masterLog;
 
+    /**
+     * the column for current name in the table view
+     */
     @FXML
     TableColumn<Log, String> currentName;
 
+    /**
+     * the column for old name in the table view
+     */
     @FXML
     TableColumn<Log, String> oldName;
 
+    /**
+     * the column for timeStamp in the table view
+     */
     @FXML
     TableColumn<Log, String> timeStamp;
 
+
+    /**
+     * Stores all revision history in an observable list
+     */
     ObservableList<Log> allRevisionHistory = FXCollections.observableArrayList();
 
     @Override
