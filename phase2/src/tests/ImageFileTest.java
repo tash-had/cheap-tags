23aae8f486f9aff46c5f2463f752ca5e9187c095
Renjie Li <renjie6li@gmail.com>
2017-11-30 21:33:26 -0500

    Add unittest for imagefile

--- phase2/src/tests/ImageFileTest.java
+++ phase2/src/tests/ImageFileTest.java
@@ -3,35 +3,46 @@ package tests;
 
 import model.ImageFile;
 import model.Tag;
-//import org.junit.Test;
-//import static org.junit.Assert.*;
-
-
+import org.junit.Test;
+import java.awt.*;
 import java.io.File;
 import java.util.ArrayList;
 
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * test all of important properties in imageFile object.
+ */
 public class ImageFileTest {
     private static final int timeOut = 3000;
 
-//    @Test (timeout = timeOut)
-//    public void testGeneralRename(){
-//        File testFile = new File("src/resources/images/logo_2.jpg");
-//        ImageFile testImage = new ImageFile(testFile);
-//        testImage.generalReName("@Tag1 logo_2");
-//        assertEquals("@Tag1 logo_2",testImage.getCurrentName());
-//    }
-//
-//    @Test (timeout = timeOut)
-//    public void testGeneralRenameWithTwoParameter(){
-//        Tag t1 = new Tag("TAG1");
-//        Tag t2 = new Tag("TAG2");
-//        ArrayList<Tag> tempList = new ArrayList<Tag>();
-//        tempList.add(t1);
-//        tempList.add(t2);
-//        File testFile = new File("src/resources/images/logo_2.jpg");
-//        ImageFile testImage = new ImageFile(testFile);
-//        testImage.generalReName("@TAG1 @TAG2 logo_2",tempList);
-//        assertEquals(t1,testImage.getTagList().get(0));
-//    }
+    @Test(timeout = timeOut)
+    public void testGeneralRename(){
+        File testFile = new File("src/resources/images/logo_2.jpg");
+        ImageFile testImage = new ImageFile(testFile);
+        testImage.generalReName("@Tag1 @Tag2 logo_2");
+        assertEquals("@Tag1 @Tag2 logo_2",testImage.getCurrentName());
+    }
+
+    @Test (timeout = timeOut)
+    public void testImageEqual(){
+        File testFile = new File("src/resources/images/logo_2.jpg");
+        File testFIle2 = new File("src/resources/images/logo_2.jpg");
+        ImageFile testImage = new ImageFile(testFile);
+        ImageFile testImage2 = new ImageFile(testFIle2);
+        assertEquals(testFile,testFIle2);
+    }
+
+    @Test (timeout = timeOut)
+    public void testImageSequence(){
+        File testFile = new File("src/resources/images/aogo_2.jpg");
+        File testFIle2 = new File("src/resources/images/logo_2.jpg");
+        ImageFile testImage = new ImageFile(testFile);
+        ImageFile testImage2 = new ImageFile(testFIle2);
+        assertTrue(testFile.compareTo(testFIle2) < 0);
+    }
+
+
 
 }
